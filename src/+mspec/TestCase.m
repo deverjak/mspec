1975e00f9128c44d9e27770faa1e5389beed8ef0
Jakub Devera <kuba.devera@seznam.cz>
2023-02-09 17:29:17 +0100

    Basic functionality of invoking is working

--- src/+mspec/TestCase.m
+++ src/+mspec/TestCase.m
@@ -1,15 +1,54 @@
 classdef TestCase < matlab.unittest.TestCase
-   
+    properties (Access = private)
+        MethodName
+        MetaMethodList
+        GroupTag
+    end
+
+    methods (Access = protected)
+        function setupTestCase(testCase)
+            testCase.setMetaDataMethodList();
+            testCase.setMethodName();
+            testCase.setGroupTag();
+            
+            methods = testCase.findMethodsToInvoke();
+            testCase.invokeTestCaseSetupMethods(methods);
+        end
+    end
 
     methods (Access = private)
-        function setupTestCase(testCase)
-            keyword = 'Given'
+        function setMethodName(testCase)
+            stack = dbstack;
+            completeMethodName = split(stack(3).name, '.');
+            testCase.MethodName = completeMethodName{2};
+        end
+        function setMetaDataMethodList(testCase)
             metaData = metaclass(testCase);
-            t = table(string({metaData.MethodList.Name})', ...
-                string({metaData.MethodList.Description})');
-            idx = find(contains(t(:,2).Variables, keyword, "IgnoreCase", true)==true);
-            fh = str2func(t(idx,1).Variables);
-            fh(testCase);
+            testCase.MetaMethodList = table(string({metaData.MethodList.Name})', ...
+                string({metaData.MethodList.Description})', 'VariableNames', ...
+                {'Method', 'Description'});
+        end
+        function setGroupTag(testCase)
+            idx = find(contains(testCase.MetaMethodList(:,1).Variables, ...
+                testCase.MethodName)==true);
+             groupDescription = extractBetween(testCase.MetaMethodList.Description(idx), ...
+                 "[", "]", "Boundaries","inclusive");
+             testCase.GroupTag = groupDescription(1);
+        end
+    
+        function methods = findMethodsToInvoke(testCase)
+            methods = string.empty;
+            for keyword = ["Given", "When"]
+                invokable = contains(testCase.MetaMethodList.Description, testCase.GroupTag) & ...
+                    contains(testCase.MetaMethodList.Description, keyword);
+                methods(length(methods)+1) = testCase.MetaMethodList.Method(invokable);
+            end
+        end
+        function invokeTestCaseSetupMethods(testCase, methods)
+            for method = methods
+                fh = str2func(method);
+                fh(testCase);
+            end
         end
     end
 end
